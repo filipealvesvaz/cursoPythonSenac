d786d0cca107cba16141b857438d7ac750932c37
Filipe Alves Vaz <filipealvesvaz@gmail.com>
2020-08-05 18:52:33 +0000

    meu segundo comit

---
+++ exemplos/tabuada.py
@@ -0,0 +1,7 @@
+tabuada = input("informe o numero desejado")
+tabuada = int(tabuada)
+
+while iterador <= 10:
+	resultado = interador * tabuada
+	print(tabuada, "x", iterador, "=", resultado)
+	iterador += 1
